d0a532292a11c114f07deefec71ae61e3f7cad0f
Wasim Juned <w@juned.net>
2018-08-13 17:37:08 +0100

    fixing tabs

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-  region  = "us-east-1"
+  region    = "us-east-1"
 }
 
 
 resource "aws_route53_zone" "example" {
-  name = "example1234512345.com"
+  name      = "example1234512345.com"
 }
 
 resource "aws_route53_record" "services" {
-  zone_id = "${aws_route53_zone.example.zone_id}"
-  name 	= "services"
-  type 	= "A"
-  ttl   = "3600"
-  records        = ["10.10.10.10"]
+  zone_id   = "${aws_route53_zone.example.zone_id}"
+  name 	    = "services"
+  type 	    = "A"
+  ttl       = "3600"
+  records   = ["10.10.10.10"]
 }
